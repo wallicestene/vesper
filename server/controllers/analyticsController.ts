dd2cc610e95e07227f789e6f50b649b6c1f44af2
wallicestene <wallicestenewaweru@gmail.com>
2025-08-27 21:34:33 +0300

    added analytics

---
+++ server/controllers/analyticsController.ts
@@ -0,0 +1,19 @@
+import prisma from "../src/prisma";
+import { Request, Response } from "express";
+
+export const getAnalytics = async (req: Request, res: Response) => {
+  try {
+    const postCount = await prisma.post.count();
+    const scheduledPosts = await prisma.post.count({
+      where: { status: "scheduled" },
+    });
+    const publishedPosts = await prisma.post.count({
+      where: { status: "published" },
+    });
+    res.status(200).json({
+      postCount,
+      scheduledPosts,
+      publishedPosts,
+    });
+  } catch (error) {}
+};
