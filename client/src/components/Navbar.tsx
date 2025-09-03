843f427c8d8b097ffcdf523d2591a607f67144e2
wallicestene <wallicestenewaweru@gmail.com>
2025-09-03 17:06:04 +0300

    navbar upgrade

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -26,42 +26,8 @@ function Navbar() {
       </div>
 
       {/* Navigation Links */}
-      <div className="nav-center hidde md:block">
-        {/* <ul className="flex space-x-1">
-          <li>
-            <Link
-              href="/features"
-              className="px-4 py-2 text-lg  text-primary-700 hover:bg-primary-50 hover:text-primary-800 rounded-md transition-all duration-200 hover:rounded-full"
-            >
-              Features
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/channels"
-              className="px-4 py-2 text-lg  text-primary-700 hover:bg-primary-50 hover:text-primary-800 rounded-md transition-all duration-200 hover:rounded-full"
-            >
-              Channels
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/resources"
-              className="px-4 py-2 text-lg  text-primary-700 hover:bg-primary-50 hover:text-primary-800 rounded-md transition-all duration-200 hover:rounded-full"
-            >
-              Resources
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/pricing"
-              className="px-4 py-2 text-lg  text-primary-700 hover:bg-primary-50 hover:text-primary-800 rounded-md transition-all duration-200 hover:rounded-full"
-            >
-              Pricing
-            </Link>
-          </li>
-        </ul> */}
-        <NavMenu/>
+      <div className="nav-center hidden md:block">
+        <NavMenu />
       </div>
 
       {/* Auth Buttons */}
@@ -74,14 +40,14 @@ function Navbar() {
         </Link>
         <Link
           href="/signup"
-          className="px-4 py-2 bg-primary-500 hover:bg-primary-600 text-white text-sm  rounded-md transition-all duration-200 shadow-sm hover:shadow-md"
+          className="px-4 py-2 bg-primary-500 hover:bg-primary-600 text-white text-sm  rounded-full transition-all duration-200 shadow-sm hover:shadow-md"
         >
           Get Started
         </Link>
       </div>
 
       {/* Mobile Menu Button */}
-      {/* <div className="md:hidden">
+      <div className="md:hidden">
         <button className="p-2 text-primary-700 hover:text-primary-800 hover:bg-primary-50 rounded-md transition-colors">
           <svg
             className="w-5 h-5"
@@ -97,7 +63,7 @@ function Navbar() {
             />
           </svg>
         </button>
-      </div> */}
+      </div>
     </nav>
   );
 }
